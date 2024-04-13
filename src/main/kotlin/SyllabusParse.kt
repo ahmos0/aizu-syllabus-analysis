c434800440d086a1eca27e755683ade872051501
HAMADA <82748182+ahmos0@users.noreply.github.com>
2024-04-13 20:40:14 +0900

    会津大のシラバスから授業コードと講義名 を取れるようにした

---
+++ src/main/kotlin/SyllabusParse.kt
@@ -0,0 +1,18 @@
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import java.io.FileWriter
+
+fun main() {
+    val url = "https://web-ext.u-aizu.ac.jp/official/curriculum/syllabus/1_J_000.html"
+
+    try {
+        val doc: Document = Jsoup.connect(url).get()
+        val links = doc.select("a[href^='2024_1_J_'][href*='.html#SS']")
+        for (link in links) {
+            FileWriter("syllabus.txt", true).use { it.write("${link.text()}\n") }
+            println(link.text())
+        }
+    } catch (e: Exception) {
+        println("Error : ${e.message}")
+    }
+}
